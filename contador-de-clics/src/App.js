c8b281d1db62e3c8fba69ec395e7a7c50ce3d453
José Manuel <josemanuel@iMac-de-Jose.local>
2023-10-04 13:52:32 +0200

    proyecto2

---
+++ contador-de-clics/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logoapp from './imagenes/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img className='freecodecamp-logo' src={logoapp} alt = 'Logo'/>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
