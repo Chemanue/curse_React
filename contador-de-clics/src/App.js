fc396d465c9fab84052cab888caac027ba708edf
Lara Romero <lararomero@MacBook-Air-de-Lara.local>
2023-10-09 22:45:37 +0200

    primeros estilos y botones

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
+import Boton from './componentes/Boton';
 import logoapp from './imagenes/logo.png';
 
 function App() {
+
+  const manejarCLic = () => {
+    console.log("Clic");
+  }
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
         <img className='freecodecamp-logo' src={logoapp} alt = 'Logo'/>
-
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarCLic}/>
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={manejarCLic}/>
       </div>
       
     </div>
